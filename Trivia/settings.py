d53a4c9833af6f8273bab66121789d5ae6a80a12
Felps <felipealcantarbv@gmail.com>
2022-02-11 15:48:33 -0600

    SECRET_KEY

--- Trivia/settings.py
+++ Trivia/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.urandom(32)
+SECRET_KEY = 'Cr+Wg+QDvs686P6HMFt4y9o1OQih3Rg9Xw96LPGlWTQ='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
