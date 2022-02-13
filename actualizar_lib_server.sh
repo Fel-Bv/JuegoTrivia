fd03aa35b4ed8a53f606d047e3bdeaab5e21e2f3
Felps <felipealcantarbv@gmail.com>
2022-02-13 10:39:37 -0600

    Actualización script

--- actualizar_lib_server.sh
+++ actualizar_lib_server.sh
@@ -2,6 +2,6 @@
 #-*- coding: utf8 -*-
 
 echo -en "\x1b[1;34mCopiando archivos... \x1b[0m"
-cp -r lib/* /app/.heroku/python/lib/python3.8/site-packages/
+cp -r lib/* /app/.heroku/python/lib/
 echo -e "\x1b[1;32mListo\x1b[0m"
 
